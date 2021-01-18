8a0d6c03b5b4d7b68ec27e751b709b93b282e703
andry1997 <Americanidiot.97>
2021-01-18 11:22:23 +0100

    add Owner Index Page and Controller. Closes #13.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
-   //private final CrudService crudService;
-
-   //public VetController(CrudService crudService) {
-   //    this.crudService = crudService;
-   //}
-
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
     public String listVets(Model model){
 

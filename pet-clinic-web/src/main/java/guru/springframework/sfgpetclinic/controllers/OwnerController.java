d2943ff32f430f32ef3a8f8e7b275ba2755c2e3b
andry1997 <Americanidiot.97>
2021-01-18 11:29:16 +0100

    add Owner Index Page and Controller update.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,11 +3,13 @@ package guru.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// Essendo che /owners verrò utilizzato all'interno degli url associti a questo controller posso porlo come generale
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"", "/index", "/index.html"})
     public String ownerList(){
 
         return "owners/index";

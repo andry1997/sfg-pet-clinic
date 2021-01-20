afef2ef4d45d619a38250fd880c73d3c12d0b6ed
andry1997 <Americanidiot.97>
2021-01-20 16:37:32 +0100

    Enhance Owners with contact info and Pets on stratup. Closes #32.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,13 +1,24 @@
 package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -21,7 +32,36 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+        if(object == null) {
+            return null;
+        }else{
+            /**
+             * Vogliamo far si che la property ID associato agli owner, pet e petType, definiti tramite una HashMap
+             * sia sincronizzato in qualche modo
+             */
+
+            if (object.getPets() != null){
+                object.getPets().forEach(pet -> {
+                    // Se abbiamo assegnato al nostro owner un animale il cui
+                    // tipo non è ancora stato aggiunto alla lista questo viene aggiunto
+                    if(pet.getPetType() != null){
+                        // L'id viene esteso dalla classe BaseEntity
+                        if (pet.getPetType().getId() == null){
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    }else{
+                        // Altrimenti avremmo owner senza un animale che non sarebbe proprio corretto logicamente parlando
+                        throw  new RuntimeException("Pet Type is required");
+                    }
+                    // abbiamo un animale di quel tipo nel nostro elenco ma non abbiamo uno specifico ID per quello specifico animale
+                    if(pet.getId() == null){
+                        Pet savePet = petService.save(pet);
+                        pet.setId(savePet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        }
     }
 
     @Override

27d53717a66b5685483e8a2026963bb4d91b9ec0
andry1997 <Americanidiot.97>
2021-01-20 15:15:38 +0100

    Create Pet Type, Pet and Visit Entities. Closes #25

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+// Va a definire una sola classe che verrà definita come Entity che poi verrà estesa dale altre
 public class BaseEntity implements Serializable {
 
     private Long id;

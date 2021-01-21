c6d567253cd10d3e80b3d5169aa60f4aa9b3ce57
andry1997 <Americanidiot.97>
2021-01-21 12:41:42 +0100

    Implement Base Entity (JPA). Closes #34.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,10 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
-// Va a definire una sola classe che verrà definita come Entity che poi verrà estesa dale altre
+// Va a definire una sola classe che verrà definita come Entity che poi verrà estesa dalle altre
+// questa classe specifica non verrà mappata nel DB
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
